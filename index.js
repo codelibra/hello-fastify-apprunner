fa036c79ce0177937123dadcd8c38d5484bf11e8
Shivendra Panicker <shivendranitb@gmail.com>
2023-02-15 20:23:24 -0800

    request timeout

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function start (opts) {
     if (err) {
       throw err
     }
-
+    app.server.requestTimeout = 1;
     app.log.info('server listening on port %d', app.server.address().port)
   })
 }
